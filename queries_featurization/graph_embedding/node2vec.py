2a602b3e745140fd03e246107b3f6e6b2d3afd67
Divyanshu Verma <divyanshu.verma1993@gmail.com>
2024-04-18 19:12:07 -0400

    update node2vec

--- queries_featurization/graph_embedding/node2vec.py
+++ queries_featurization/graph_embedding/node2vec.py
@@ -144,9 +144,6 @@ def alias_draw(J, q):
 
 	kk = int(np.floor(np.random.rand()*K))
 	if np.random.rand() < q[kk]:
-	    return kk
-	    return kk
+		return kk
 	else:
-	  return kk
-	else:
-	return J[kk]
+		return J[kk]
