e65693a718a29022f3c35f8f9cf47f22d79223ee
Erostrate9 <ericsun42@outlook.com>
2024-08-16 09:11:09 +0000

    built column rdc matrix

--- queries_featurization/build_graphs/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Implements the Randomized Dependence Coefficient
-David Lopez-Paz, Philipp Hennig, Bernhard Schoelkopf
-
-http://papers.nips.cc/paper/5138-the-randomized-dependence-coefficient.pdf
-"""
-
-from .rdc import rdc
